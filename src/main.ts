737f35897b52489fd69200bd2965d77164e274a0
Ronnie Vega <9502820+ronnie-codes@users.noreply.github.com>
2024-09-11 21:20:13 +0000

    Remove cats tag

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ const bootstrap = async () => {
     .setTitle('Youtube API')
     .setDescription('Youtube API and more')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
