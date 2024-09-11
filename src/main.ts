1596f799332bffe1f153ebf39df994f99c247f7f
Ronnie Vega <9502820+ronnie-codes@users.noreply.github.com>
2024-09-11 21:17:29 +0000

    Setup swagger.

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,32 @@ import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import { Innertube } from 'youtubei.js';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const bootstrap = async () => {
-  dotenv.config({ path: `.config/${process.env.NODE_ENV}.env` });
+  // Setup root composition
   const innertube = await Innertube.create();
   const app = await NestFactory.create(AppModule.create(innertube));
+
+  // Setup Globals
   app.useGlobalPipes(new ValidationPipe({ validateCustomDecorators: true }));
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Youtube API')
+    .setDescription('Youtube API and more')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Setup Environment
+  dotenv.config({ path: `.config/${process.env.NODE_ENV}.env` });
   const configService = app.get(ConfigService);
   const port = process.env.PORT || configService.get('PORT') as string | number;
+
+  // Listen
   await app.listen(port);
 };
 
